954b71a6aac6570ff75d6a3ac789b1c6e8f982f0
Daniel Snitkovskiy <snitkdan@uw.edu>
2020-05-14 18:15:22 -0700

    add part1

--- LoadBalancers/load_balancer_simple.py
+++ LoadBalancers/load_balancer_simple.py
@@ -13,16 +13,34 @@ class SimpleLoadBalancer(LoadBalancer):
     # Adds a shard to the system and rebalance as necessary
     def add_shard(self, shard_name):
         super().add_shard(shard_name)
-        # TODO rebalance
-        raise NotImplementedError
+        self.rebalance() # all keys need to be re-mapped 
 
     # Remove a shard to the system and rebalance as necessary
     def remove_shard(self, shard_name):
         kvstore = super().remove_shard(shard_name)
-        # TODO rebalance
-        raise NotImplementedError
+        self.rebalance() # all keys need to be re-mapped
+        self.rekey(kvstore, shard_name) # re-assigns keys in removed shard
+   
+    # Re-Assigns the keys in the passed in kvstore
+    def rekey(self, kvstore, shard_name):
+        for key in list(kvstore.keys()):
+            targetShardName = self.getShardNameForKey(key)
+            if targetShardName != shard_name: # have to move this key
+                val = kvstore.pop(key)
+                targetShard = self.shards[targetShardName]
+                targetShard.put(key, val)
+
+    # Re-assigns all of the keys in the current configuration
+    def rebalance(self):
+        for (shard_name, shard) in self.shards.items():
+            self.rekey(shard.kvstore, shard_name)
+
+    # Gets the shard assigned to this key
+    def getShardNameForKey(self, key):
+        shardNum = ord(key[0]) % self.num_shards
+        return self.shard_list[shardNum]
 
     # Puts a key in a certain shard, incrementing the access count
     def put(self, k):
-        # TODO Figure out where to put it
-        raise NotImplementedError
+        shard = self.shards[self.getShardNameForKey(k)] 
+        shard.put(k, 1)
--- WRITEUP.md
+++ WRITEUP.md
@@ -1,12 +1,21 @@
 # Short response
-Name: 
-UWNetID: 
+Name: Daniel Snitkovskiy
+
+UWNetID: snitkdan
 
 ## Part 1: Simple Load Balancer
 Q1: What does the distribution look like? 
 
+> Moved >90% of the keys >5 million times (when I ran the test last). There is a high variance in key movement (~81 million), but with a mean of ~2,000. This looks like on average, not many keys were moved, but
+volatility based on the workload was substantial, making this not a 
+very stable load balancer. 
 
 Q2: What might be a problem with this type of load balancing scheme?
+
+> One problem with this scheme is that some starting letters are more common than others (depending on the language of the key of-course). 
+This will make certain keys more popular than others just based on
+the first letter, which could leave servers assigned to common
+key prefixes overwhelmed. 
 
 
 ## Part 2: The Key is Hashing
